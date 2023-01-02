fb23b9ccf6437519b381a2db801d2c59317dd5bf
Your Namekshai-7 <akshai2537@gmail.com>
2023-01-02 20:29:34 +0530

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,181 +11,218 @@
     <link rel="stylesheet" href="/resources/demos/style.css">
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <link rel="stylesheet" href="">
 </head>
 <style>
+
     body{
         font-family: 'Times New Roman', Times, serif
     }
+.nav{
+    color: white;
+}
     .container{
-        margin-top: 20px;
-        width: 90%;
-        border: 1px solid lightgray;
-        height: 100%;
+        margin-top: 40px;
+        width: 100%;
+        border: px solid lightgray;
+        height: 630px;
 
     }
     .box{
-        width: 80%;
+        width: 50%;
         height: 100%;
         display: flex;
         margin-left: 10px;
+        margin-top: 20px;
+        font-size: 15px;
 
     }
     .box1{
-        margin-top: 10px;
-        margin-left: 30px;
+        width: 950px;
+        margin-left: 0px;
+        font-size: 17px;
     }
+
     .footer {
        position: fixed;
        left: 0;
        bottom: 0;
        width: 100%;
-       background-color: gray;
+
        color: white;
        text-align: center;
     }
     </style>
 <body>
 <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg   bg-secondary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
+          <a class="navbar-brand nav" href="#">Grocery shop</a>
         </div>
+        <div><a href="/" class="navbar-brand nav"><h5>LogOut</h5></a></div>
       </nav>
 </header>
 <section class="container">
-    <form action="index.php" method="POST" autocomplete="off">
-        <div class="box">
-                <div class="col-md-5">
-                    <h5 class="text-success">Bill Details</h5>
-                    <div class="form-group">
-                        <label for="">Bill No</label>
+    <form action=".php" method="POST" autocomplete="off">
+        <div class="box ">
+                <div class="col-md-5 ">
+                    <h5 class="text-primary">Bill Details</h5>
+                    <div class="form-group col-sm-5">
+                        <label>Bill No</label>
                         <input type="text" name="invoice_no" required class="form-control">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group col-sm-5 ">
                         <label for="">Date</label>
                         <input type="text" name="invoice_date" id="date" required class="form-control">
                     </div>
+                    <hr class="col-sm-7">
+                           <h5 class="text-primary">Customer Details</h5>
+                            <div class="form-group col-sm-8">
+                                <label for="">Name</label>
+                                <input type="text" name="cname" required class="form-control">
+                            </div>
+                            <div class="form-group col-sm-8">
+                                <label for="">Address</label>
+                                <input type="text" name="caddress" required class="form-control">
+                            </div>
+                            <div class="form-group col-sm-8">
+                                <label for="">Mobile</label>
+                                <input type="text" name="mobile" required class="form-control">
+                            </div>
                 </div>
-                <div class="col-md-5">
-                <h5 class="text-success">Customer Details</h5>
-                    <div class="form-group">
-                        <label for="">Name</label>
-                        <input type="text" name="cname" required class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="">Address</label>
-                        <input type="text" name="caddress" required class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="">City</label>
-                        <input type="text" name="ccity" required class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="">Mobile</label>
-                        <input type="text" name="mobile" required class="form-control">
-                    </div>
-                </div>
-            </div>
-            <div class="box1">
-                <div class="col-md-10">
-                <h5 class="text-primary">Product Details</h5>
-                <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Product</th>
-                        <th>Price</th>
-                        <th>Qty</th>
-                        <th>Tax</th>
-                        <th>Tax.amount</th>
-                        <th>Total</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody id='row'>
-                    <tr>
-                        <td><input type="text" required name="pname" class="form-control"></td>
-                        <td><input type="text" required name="price" class="form-control price"></td>
-                        <td><input type="text" required name="qty" class="form-control qty"></td>
-                        <td><input type="text" required name="tax" class="form-control tax"></td>
-                        <td><input type="text" required name="tax.amount" class="form-control tax.amount"></td>
-                        <td><input type="text" required name="total" class="form-control total"></td>
+            <div>
+                <div class="box1">
+                    <h5 class="text-primary">Product Details</h5>
+                    <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th class="col-md-1" style="text-align:center;">S.no</th>
+                            <th style="text-align:center;" class="col-md-3">Product</th>
+                            <th style="text-align:center;" >Price</th>
+                            <th style="text-align:center;">Qty</th>
+                            <th style="text-align:center;">Sub Total</th>
+                            <th style="text-align:center;">Tax(%)</th>
+                            <th style="text-align:center;">Tax-amount</th>
+                            <th style="text-align:center;">Total</th>
+                            <th style="text-align:center;"><input type="button" value="+" class=" btn btn-primary " id=btn-add-row></th>
+                        </tr>
+                    </thead>
+                    <tbody id='row'>
+
+                        <tr>
+                            <td><input type="text" required name="sno" class="form-control" style="text-align:center;" value="1"> </td>
+                            <td ><input type="text" required name="pname" class="form-control" style="text-align:center;"></td>
+                            <td><input type="text" required name="price" class="form-control price" style="text-align:center;"></td>
+                            <td><input type="text" required name="qty" class="form-control qty" style="text-align:center;"></td>
+                            <td><input type="text" required name="subtotal" class="form-control subtotal" style="text-align:center;"></td>
+                            <td><input type="text" required name="tax" class="form-control tax" style="text-align:center;"></td>
+                            <td><input type="text" required name="tax.amount" class="form-control taxamount" style="text-align:center;"></td>
+                            <td><input type="text" required name="total" class="form-control total" style="text-align:center;"></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" required name="sno" class="form-control" style="text-align:center;" value="2"></td>
+                            <td><input type="text" required name="pname" class="form-control" style="text-align:center;"></td>
+                            <td><input type="text" required name="price" class="form-control price" style="text-align:center;"></td>
+                            <td><input type="text" required name="qty" class="form-control qty" style="text-align:center;"></td>
+                            <td><input type="text" required name="subtotal" class="form-control subtotal" style="text-align:center;"></td>
+                            <td><input type="text" required name="tax" class="form-control tax" style="text-align:center;"></td>
+                            <td><input type="text" required name="tax.amount" class="form-control taxamount" style="text-align:center;"></td>
+                            <td><input type="text" required name="total" class="form-control total" style="text-align:center;"></td>
+                            <td><input type="button" value="X" class="btn btn-danger btn-sm btn-row-remove" style="text-align:center;"></td>
+                        </tr>
+
+                    </tbody>
+                    <tfoot>
+                    <tr class="new">
+                        <td colspan="6"></td>
+                        <td style="text-align:center;"><B>Grand Total</B></td>
+                        <td><input type="text" name="grand_total" id="grand_total" class="form-control" required></td>
                         <td></td>
                     </tr>
-                    <tr>
-                        <td><input type="text" required name="pname" class="form-control"></td>
-                        <td><input type="text" required name="price" class="form-control price"></td>
-                        <td><input type="text" required name="qty" class="form-control qty"></td>
-                        <td><input type="text" required name="tax" class="form-control tax"></td>
-                        <td><input type="text" required name="tax.amount" class="form-control tax.amount"></td>
-                        <td><input type="text" required name="total" class="form-control total"></td>
-                        <td><input type="button" value="X" class="btn btn-danger btn-sm btn-row-remove"></td>
-                    </tr>
-
-                </tbody>
-                <tfoot>
-                  <tr class="new">
-                    <td><input type="button" value="+Add Row" class=" btn btn-primary btn-sm" id=btn-add-row></td>
-                    <td colspan="4"><a colspan="2"> Total</a></td>
-                    <td><input type="text" name="grand_total" id="grand_total" class="form-control" required></td>
-                    <td></td>
-                  </tr>
-                </tfoot>
-                </table>
-                <input type="submit" name="submit" value="Save Invoice" class="btn btn-success float-right">
+                    </tfoot>
+                    </table>
+                    <p align="right"><input type="submit" name="submit" value="Submit" class="btn btn-primary"></p>
                 </div>
             </div>
+        </div>
    </form>
-</div>
+
 <script>
-    $(document).ready(function(){
+        $(document).ready(function(){
         $("#date").datepicker({
             dateFormat:"dd-mm-yy"
         });
+
 // <--Add row-->
         $("#btn-add-row").click(function(){
-        $("#row").append("<tr><td><input type='text' required name='pname' class='form-control'></td> <td><input type='text' required name='price' class='form-control price'></td><td><input type='text' required name='qty' class='form-control qty'></td><td><input type='text' required name='tax' class='form-control tax'></td><td><input type='text' required name='tax.amount' class='form-control tax.name'></td><td><input type='text' required name='total[]' class='form-control total'></td> <td><input type='button' value='X' class='btn btn-danger btn-sm btn-row-remove'></td></tr>");
+        $("#row").append("<tr>  <td><input type='text' required name='sno' class='form-control' value='' style='text-align:center;'></td><td><input type='text' required name='pname' class='form-control' style='text-align:center;'></td> <td><input type='text' required name='price' class='form-control price' style='text-align:center;'></td><td><input type='text' required name='qty' class='form-control qty' style='text-align:center;'></td><td><input type='text' required name='subtotal' class='form-control subtotal' style='text-align:center;'></td><td><input type='text' required name='tax' class='form-control tax' style='text-align:center;'></td><td><input type='text' required name='taxamount' class='form-control taxamount' style='text-align:center;'></td> <td><input type='text' required name='total' class='form-control total' style='text-align:center;'></td><td><input type='button' value='X' class='btn btn-danger btn-sm btn-row-remove' style='text-align:center;'></td></tr>");
         });
+
 
 // <--Del row-->
-        $('tbody').on('click','.btn-row-remove',function(){
+        $("tbody").on("click",".btn-row-remove",function(){
         alert("Are You Sure?");
         $(this).closest( 'tr').remove();
+        grand_total()
+        });
+// <--subtotal-->
+        $("tbody").on("keyup",".price",function(){
+            var price=Number($(this).val());
+            var qty =Number($(this).closest("tr").find(".qty").val());
+            $(this).closest("tr").find(".subtotal").val(price*qty);
+            grand_total()
+        });
+
+        $("tbody").on("keyup",".qty",function(){
+            var price =Number($(this).closest("tr").find(".price").val());
+            var qty=Number($(this).val());
+            $(this).closest("tr").find(".subtotal").val(price*qty);
+            grand_total()
+
+        });
+// <--taxamount-->
+        $("tbody").on("keyup",".subtotal",function(){
+            var subtotal=Number($(this).val());
+            var tax =Number($(this).closest("tr").find(".tax").val());
+            $(this).closest("tr").find(".taxamount").val(subtotal*tax/100);
+            grand_total()
+        });
+
+        $("tbody").on("keyup",".tax",function(){
+            var tax=Number($(this).val());
+            var subtotal =Number($(this).closest("tr").find(".subtotal").val());
+            $(this).closest("tr").find(".taxamount").val(subtotal*tax/100);
+            grand_total()
+        });
+// <--total-->
+        $("tbody").on("keyup",".subtotal",function(){
+            var subtotal=Number($(this).val());
+            var taxamount =Number($(this).closest("tr").find(".taxamount").val());
+            $(this).closest("tr").find(".total").val(subtotal+taxamount);
+            grand_total()
+        });
+        $("tbody").on("keyup",".taxamount",function(){
+            var taxamount=Number($(this).val());
+            var subtotal =Number($(this).closest("tr").find(".subtotal").val());
+            $(this).closest("tr").find(".total").val(subtotal+taxamount);
+            grand_total()
+        });
+// <--grand_total-->
+        function grand_total(){
+            var total=0;
+            $(".total").each(function(){
+                total += Number($(this).val());
+            });
+            $("#grand_total").val(total);
+        }
+
+
         });
 
 
 
-    });
+
 
 
 
@@ -193,8 +230,8 @@
 </script>
 </section>
 
-<footer>
-    <div class="footer">
+<footer class="bg-secondary">
+    <div class="footer bg-secondary">
         <p>Footer</p>
       </div>
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,33 +4,102 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Grocery-Billing</title>
+    <title>Billing</title>
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
-<body>
-    <div class="container custom-login">
-        <div class="row">
-            <div class="col-sm-4 col-sm-offset-4">
-                <form role="form" action="/index" method="POST">
-                    @csrf
-                    <div class="form-group">
-                      <label for="exampleInputEmail1">Email address</label>
-                      <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" required>
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputPassword1">Password</label>
-                      <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
-                    </div>
-                    <button type="submit" class="btn btn-default">Login</button>
-                  </form>
-            </div>
-        </div>
-     </div>
+<style>
+    body{
+        font-family: 'Times New Roman', Times, serif;
+        height: 100vh;
+        width: 100vw;
+    }
+    .container{
+        margin-top: 10%;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .row{
+        border: 1px solid lightgray;
+        width: 400px;
+        height: 400px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .box{
+        margin-top: 10px
+    }
+    .input-container {
+    display: -ms-flexbox; /* IE10 */
+    display: flex;
+    width: 100%;
+    margin-bottom: 15px;
+    }
 
+    .icon {
+    padding: 10px;
+    text-align: center;
+    }
+    .nav{
+    color: white;
+    height: 50px;
+}
+
+    .footer {
+       position: fixed;
+       left: 0;
+       bottom: 0;
+       width: 100%;
+
+       color: white;
+       text-align: center;
+    }
+
+</style>
+<body>
+    <header>
+        <nav class="bg-secondary nav">
+            <div class="">
+              <a class="navbar-brand" href=""><B>Grocery shop</B> </a>
+            </div>
+          </nav>
+    </header>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 box">
+                    <form role="form" action="/index" method="POST">
+                        @csrf
+                            <div class="input-container">
+                                <i class="fa fa-envelope icon bg-primary"></i>
+                                <input class="input-field form-control" type="text" placeholder="Enter Email" name="email">
+                            </div>
+
+                            <div class="input-container ">
+                                <i class="fa fa-key icon bg-primary"></i>
+                                <input class="input-field form-control" type="password" placeholder="Password" name="password">
+                            </div>
+                            <p align="center"><input type="submit" name="submit" value="Login" class="btn btn-primary"></p>
+                      </form>
+                </div>
+            </div>
+         </div>
+    </section>
+    <footer class="bg-secondary">
+        <div class="footer bg-secondary">
+            <p>Footer</p>
+          </div>
+
+    </footer>
 </body>
 </html>
